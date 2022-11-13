1d20d5d366fcb6194c6e7acc5ba8d78a23624e3a
Trojanku <trojan.kub@gmail.com>
2022-11-13 19:58:32 +0100

    add newsletter routes

--- server/routes.go
+++ server/routes.go
@@ -1,8 +1,21 @@
 package server
 
-import "Goo/handlers"
+import (
+	"Goo/handlers"
+	"Goo/model"
+	"context"
+)
 
 func (s *Server) setupRoutes() {
 	handlers.Health(s.mux)
 	handlers.FrontPage(s.mux)
+
+	handlers.NewsletterSignup(s.mux, &signupperMock{})
+	handlers.NewsletterThanks(s.mux)
+}
+
+type signupperMock struct{}
+
+func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
+	return "", nil
 }
